90c1550a3edfa398e8bb0fcefd51c81e5b4ab56f
pcorbett <patrick@redfinsolutions.com>
2021-03-22 15:53:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def add_client():
     new_client = request.args.get('client')
     token_v2 = os.environ.get("TOKEN")
     url = os.environ.get("URL")
+    dump()
     createNotionTask(token_v2, url, new_client)
     return f'added {new_client} to Notion'
 
